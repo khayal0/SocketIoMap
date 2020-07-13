6938e03e79a2abebd332a29bfc43075de3f90f86
Xayal V. Isaxanov <IsaxanovXV@kapitalho.az>
2020-07-13 09:03:42 +0400

    init

---
+++ app/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+
+import GeoLocation from "./GeoLocation";
+
+const ENDPOINT = "http://127.0.0.1:4001";
+
+function App() {
+  const [response, setResponse] = useState("");
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    handleConnect();
+    // CLEAN UP THE EFFECT
+    return () => socket.disconnect();
+    //
+  }, []);
+
+  const handleDisconnect = () => {
+    const socket = socketIOClient(ENDPOINT);
+    console.log("disconnected");
+    socket.disconnect();
+  };
+  const handleConnect = () => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.connect();
+    socket.on("FromAPI", (data) => {
+      setResponse(data);
+    });
+  };
+  return (
+    <p>
+      It's <time dateTime={response}>{response}</time>
+      <button onClick={handleDisconnect}>GO OFFLINE</button>
+      <button onClick={handleConnect}>GO ONLINE</button>
+      <GeoLocation watchPosition={true} />
+    </p>
+  );
+}
+
+export default App;
